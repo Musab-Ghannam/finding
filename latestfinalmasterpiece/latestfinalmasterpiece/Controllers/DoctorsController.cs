aa9491cf26abdc57fb36360f09eee98512e8aaa3
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2023-02-21 10:38:09 -0800

    21 2

---
+++ latestfinalmasterpiece/latestfinalmasterpiece/Controllers/DoctorsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace latestfinalmasterpiece.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        // GET: Doctors
+        public ActionResult alldoctors()
+        {
+            return View();
+        }
+
+
+        public ActionResult singledoctor()
+        {
+            return View();
+        }
+
+        public ActionResult doctorprofile()
+        {
+            return View();
+        }
+
+        public ActionResult Doctorprof()
+        {
+            return View();
+        }
+
+        public ActionResult booking()
+        {
+            return View();
+        }
+
+
+        public ActionResult checkout()
+        {
+            return View();
+        }
+    }
+}
